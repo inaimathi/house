0290b848947e63c66bf1c37013dafe969fd61321
inaimathi <inaimathi@self>
2014-06-04 22:32:34 -0400

    -Incorporated notes from [Doug Hoyte's Perl implementation](https://github.com/hoytech/Session-Token)

--- session.lisp
+++ session.lisp
@@ -10,24 +10,31 @@
 (defun clear-session-hooks! ()
   (setf *new-session-hook* nil))
 
-(let ((ctx nil))
-  (defun new-session-token ()
+(let* ((ctx nil)
+       (alphabet (char-range (#\a :to #\z #\A :to #\Z #\0 :to #\9)))
+       (token-length 64)
+       (next-pow (next-power-of-two (length alphabet))))
+  (defun next-char ()
+    (loop for b = (rand-bits ctx next-pow) until (> (length alphabet) b)
+       finally (return b)))
+  
+  (defun new-session-token! ()
     (unless ctx
-      (setf ctx
-	    (if (or (cl-fad:file-exists-p "/dev/urandom")
-		    (cl-fad:file-exists-p "/dev/arandom"))
-		(init-kernel-seed)
-		(progn 
-		  (warn "neither /dev/urandom nor /dev/arandom found; using insecure session tokens")
-		  (init-common-lisp-random-seed)))))
-    (format nil "session=~32,'0x" (rand-bits ctx 384))))
+      (setf ctx 
+	    #-win32 (init-kernel-seed)
+	    #+win32 (progn (warn "Running on Windows; using insecure session tokens")
+			   (init-common-lisp-random-seed))))
+    (let ((buf (make-string token-length)))
+      (loop for i from 0 repeat token-length
+	 do (setf (aref buf i) (aref alphabet (next-char))))
+      buf)))
 
 (let ((session-count 0))
   (defun new-session! ()
     (when (>= (incf session-count) +clean-sessions-every+)
       (setf session-count 0)
       (clean-sessions!))
-    (let ((session (make-instance 'session :token (new-session-token))))
+    (let ((session (make-instance 'session :token (new-session-token!))))
       (setf (gethash (token session) *sessions*) session)
       (loop for hook in *new-session-hook*
 	 do (funcall hook session))
--- util.lisp
+++ util.lisp
@@ -34,6 +34,19 @@
 	       (dbg "Publishing to channel" chan msg))
     nil))
 
+(defmethod next-power-of-two ((num number))
+  (loop for i from 1 for pow = (expt 2 i)
+     when (>= pow num) return i))
+
+(defmacro char-range ((&rest start-to-end-pairs) &key (plus) (not))
+  (let ((res))
+    (loop for (a to b) on start-to-end-pairs by #'cdddr
+       do (let ((min (min (char-code a) (char-code b)))
+		(max (max (char-code a) (char-code b))))
+	    (loop for code from min to max for c = (code-char code)
+	       unless (find c not) do (push c res))))
+    (coerce (nreverse (append (set-difference plus not) res)) 'string)))
+
 (defmethod ->keyword ((thing symbol))
   (intern (symbol-name thing) :keyword))
 
