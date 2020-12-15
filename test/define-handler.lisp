9d6cbd54dbffa9a9e20e59817240a5db79fc259f
inaimathi <leo.zovic@gmail.com>
2020-12-15 09:57:00 -0500

    Update tests

--- test/define-handler.lisp
+++ test/define-handler.lisp
@@ -2,30 +2,37 @@
 
 (tests
  (subtest
-  "Request get-arg"
+  "Request get-param"
   (let ((req (make-instance
 	      'request :parameters
 	      `((:a . "foo") (:b . "bar") (:c . "1337")
 		(:d . ,(quri:url-encode "A longer & \"fully-encoded\" string"))
 		(:e . "{\"a\": 1, \"b\": 2}")
 		(:f . "[1, 2, 3]") (:g . "[\"foo\", \"bar\"]")))))
-    (is (get-arg req :a #'>>string) "foo")
-    (is (get-arg req :b #'>>keyword) :bar)
-    (is (get-arg req :c #'>>integer) 1337)
-    (is (get-arg req :d #'>>string)
+    (is (get-param req :a #'>>string) "foo")
+    (is (get-param req :b #'>>keyword) :bar)
+    (is (get-param req :c #'>>integer) 1337)
+    (is (get-param req :d #'>>string)
 	"A longer & \"fully-encoded\" string")
-    (is (get-arg req :e #'>>json)
+    (is (get-param req :e #'>>json)
 	'((:a . 1) (:b . 2)))
-    (is (get-arg req :f (>>list #'>>integer))
+    (is (get-param req :f (>>list #'>>integer))
 	(list 1 2 3))
-    (is (get-arg req :g (>>list #'>>keyword))
+    (is (get-param req :g (>>list #'>>keyword))
 	(list :foo :bar))
-    (is-error (get-arg req :nonexistant-arg #'>>string) 'http-assertion-error)
-    (is-error (get-arg req :a #'>>integer ) 'http-assertion-error)))
+    (is-error (get-param req :nonexistant-arg #'>>string) 'http-assertion-error)
+    (is-error (get-param req :a #'>>integer ) 'http-assertion-error)))
 
  (subtest
-  "check-for-dupes"
-  ;; correctly errors on collisions
-  ;; correctly passes on no collisions
-  ;; collapses non-conflicting duplicates
-  ))
+  "dedupe-params"
+  (is-error (dedupe-params '((A >>INTEGER) (A >>KEYWORD))) 'error)
+  (is-error (dedupe-params
+	     '((A >>INTEGER) (A >>KEYWORD)
+	       (B >>INTEGER) (B >>KEYWORD)))
+	    'error)
+  (is (dedupe-params '((A >>KEYWORD) (B (>>LIST >>INTEGER))))
+      '((A >>KEYWORD) (B (>>LIST >>INTEGER))))
+  (is (dedupe-params '((A >>KEYWORD) (A >>KEYWORD)))
+      '((A >>KEYWORD)))
+  (is (dedupe-params '((A >>KEYWORD) (A)))
+      '((A >>KEYWORD)))))
--- test/package.lisp
+++ test/package.lisp
@@ -2,4 +2,9 @@
 
 (defpackage #:house/test
   (:use #:cl #:house #:test-utils)
-  (:import-from #:house #:parse #:parse-params #:http-assertion-error))
+  (:import-from
+   #:house
+
+   #:parse #:parse-params #:http-assertion-error
+   #:path-var? #:var-key #:var-annotation #:>>string
+   #:get-param #:dedupe-params))
